6f37925adf97b8edf9f000a3e2b9c5c8bab4fefa
Ashutosh Dubey <dubeyaashutosh1@gmail.com>
2024-03-16 09:26:32 +0530

    Create PalindromeString.java

---
+++ DSA_JAVA/String/PalindromeString.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class PalindromeString
+{
+
+public static boolean palindrome(String str)
+{
+int n=str.length();
+for(int i=0;i<n/2;i++)
+{
+    if(str.charAt(i)!=str.charAt(n-1-i))
+    return false;
+
+}
+return true;
+}
+public static void main(String args[])
+{
+    String str= "RACECAR";
+    System.out.println(palindrome(str));
+
+}
+}
+
+/*
+output: true
+ */
