6c98211995662eb6aec2aea4467818370180a378
Ashutosh Dubey <dubeyaashutosh1@gmail.com>
2024-03-29 08:43:18 +0530

    learning

---
+++ DSA_JAVA/String/ImmutableString.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ImmutableString
+{
+    public static void main(String args[])
+    {
+        String str="Tony";
+        for(char i='a';i<='z';i++)
+        {
+            str+=i;
+        }
+    System.out.println(str);
+    }
+}
