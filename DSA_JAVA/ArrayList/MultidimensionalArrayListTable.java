bd3eba190faad1725f7800dfee4db015a6215fbf
Ashutosh Dubey <dubeyaashutosh1@gmail.com>
2024-04-03 00:02:55 +0530

    .

--- DSA_JAVA/ArrayList/MultidimensionalArrayListTable.java
+++ DSA_JAVA/ArrayList/MultidimensionalArrayListTable.java
@@ -3,28 +3,36 @@ import java.util.ArrayList;
 public class MultidimensionalArrayListTable{
     public static void main(String[] args) {
         
-        ArrayList <Integer> mainList=new ArrayList<>();
+        ArrayList <ArrayList<Integer>> mainList=new ArrayList<>();
 
         ArrayList <Integer> list1=new ArrayList<>();
+        ArrayList <Integer> list2=new ArrayList<>();
+        ArrayList <Integer> list3=new ArrayList<>();
+
         for(int i=1;i<=5;i++){
-            list1.add(2*i);
+            list1.add(i*1);//1,2,3,4,5
+            list2.add(i*2);//2,4,8,10
+            list3.add(i*3);//3,6,9,12,15
         }
         mainList.add(list1);
-
-        ArrayList <Integer> list2=new ArrayList<>();
-        for(int j=1;j<=5;j++){
-            list2.add(2*j);
-        }
         mainList.add(list2);
-
-        ArrayList <Integer> list3=new ArrayList<>();
-        for(int k=1;k<=5;k++){
-            list3.add(2*k);
-        }
-
         mainList.add(list3);
 
-        System.out.println(mainList);
+        System.out.println(mainList);//[[1, 2, 3, 4, 5], [2, 4, 6, 8, 10], [3, 6, 9, 12, 15]]
+
+        for(int i=0;i<mainList.size();i++){//accesses mainList
+            ArrayList<Integer>currentList=mainList.get(i);//this gives full ith list
+            for(int j=0;j<currentList.size();j++){
+                System.out.print(currentList.get(j)+" ");
+            }
+            System.out.println();
+        }
 
     }
 }
+/*Output: 
+[[1, 2, 3, 4, 5], [2, 4, 6, 8, 10], [3, 6, 9, 12, 15]]
+1 2 3 4 5
+2 4 6 8 10
+3 6 9 12 15
+ */
