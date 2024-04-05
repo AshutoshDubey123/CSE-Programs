89e40559b00ff46d52aa877a641765d505a625e0
Ashutosh Dubey <dubeyaashutosh1@gmail.com>
2024-04-05 20:32:54 +0530

    .

--- CP/leetcode/Hashing/575. Distribute Candies.java
+++ CP/leetcode/Hashing/575. Distribute Candies.java
@@ -27,9 +27,9 @@ Explanation: Alice can only eat 4 / 2 = 2 candies. Even though she can eat 2 can
 Constraints:
 
 n == candyType.length
-2 <= n <= 104
+2 <= n <= 1^4
 n is even.
--105 <= candyType[i] <= 105
+-10^5 <= candyType[i] <= 10^5
  */
 class Solution {
     public int distributeCandies(int[] candyType) {
